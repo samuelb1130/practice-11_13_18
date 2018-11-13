a2f2f2934efacef6218264f96334eb6fe33d2829
sam bauer <42975601+samuelb1130@users.noreply.github.com>
2018-11-13 12:45:42 -0500

    Add files via upload

---
+++ Practice.java
@@ -0,0 +1,34 @@
+
+public class Practice {
+
+	public static void main(String[] args) {
+
+	
+	/**
+	*
+	*@ Samuel Bauer 22	
+	*	
+	**/	
+		
+		int x = 5;
+		int y = 7;
+		
+		//System.out.print(x>2 && y<6);       false
+		
+		//System.out.print(x!=5 || x !=8);    true / if one or the other is true its true
+													// will be in iif statements
+		
+		if(x<1 || x<12)
+		
+		
+		
+		
+		
+		
+		
+		
+		
+
+	}
+
+}
